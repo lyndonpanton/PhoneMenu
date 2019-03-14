74486150c20b308aca30315b5fe3876d5c8485d9
Lyndon Panton <LyndonPanton@gmail.com>
2019-03-14 23:12:14 +0000

    Added logic for highlighting selected tab

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let navItems = document.getElementsByClassName("nav-item");
+Array.from(navItems).forEach(function(item) {
+	item.addEventListener("click", function() {
+		for (let i = 0; i < Array.from(navItems).length; i++) {
+			navItems[i].classList.remove("selected");
+		}
+
+		item.classList.add("selected");
+	});
+});
